b741412b674b500c2e1e6f159053cc2fead2efe3
Kevin Nelson <km.nelson@outlook.com>
2024-08-17 16:21:17 -0400

    added apiCall function

---
+++ src/utils/apiCalls.js
@@ -0,0 +1,17 @@
+// apiCalls.js
+export const fetchNewsData = async () => {
+    const url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=275cabb19b344e0e804ee93d9e536967';
+    
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      return data.articles; // Return only the articles array
+    } catch (error) {
+      console.error('Error fetching news data:', error);
+      throw error;
+    }
+  };
+  
