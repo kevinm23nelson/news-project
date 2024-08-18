fa6e4e68362e134615b27f8fda248719f440d6d9
Kevin Nelson <km.nelson@outlook.com>
2024-08-18 04:29:25 -0400

    refactors/styling

--- src/Components/NotFoundPage/NotFoundPage.js
+++ src/Components/NotFoundPage/NotFoundPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './NotFoundPage.css'; // Ensure to create this CSS file
+import './NotFoundPage.css';
 
 function NotFoundPage() {
   return (
